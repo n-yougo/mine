4e8f3d6649f67bbe041f949b1bb73cefc819a5ae
yougon <36183417+n-yougo@users.noreply.github.com>
2020-01-17 15:09:03 +0900

    Update __init__.py

--- lab/__init__.py
+++ lab/__init__.py
@@ -1,7 +1,7 @@
 """skrvm implements RVM models."""
 
-from .rvm import RVR, RVC
+from .nyc import nyc#RVR, RVC
 
-__all__ = ['RVR', 'RVC']
+__all__ = ['nyc']#['RVR', 'RVC']
 
 __version__ = '0.1.0a1'
